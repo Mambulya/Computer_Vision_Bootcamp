84d8b53b659a5a7327d1802a33627cd6d90a9c3e
Mambulya <68922098+Mambulya@users.noreply.github.com>
2025-04-18 12:24:34 +0300

    updating lines clicking

--- src/lab05/clicking_lines.py
+++ src/lab05/clicking_lines.py
@@ -9,11 +9,11 @@ im = pylab.array(img)
 pylab.imshow(im)
 
 # зададим изначальные точки
-x = [500, 500, 700, 800]
-y = [1100, 1000, 400, 800]
+x = [500, 500, 800, 800]
+y = [1100, 800, 1100, 800]
 
 # plotting points
-pylab.plot(x, y, "r*")
+pylab.plot(x, y, "y*")
 
 # getting points
 print("Plese, click 2 points")
@@ -24,5 +24,6 @@ print(points)
 # plotting lines
 pylab.axis("off")
 pylab.title("Plotting lines")
+pylab.plot(x, y, "y-")
 pylab.plot([points[0][0], points[1][0]], [points[0][1], points[1][1]], "w--")
 pylab.show()
